dfe75939f1f5772c853cd9008fc403326487bb8b
katieamazing <k.allen@live.com>
2017-08-08 23:10:40 -0400

    :rose:

--- picography-1.0/katie.py
+++ picography-1.0/katie.py
@@ -1,8 +1,10 @@
+
+import sys
 from picography import *
 
 def katie():
 
-    timespan("1988-04-08", "2017-08-01", year_height=52, legend=True)
+    timespan("1988-04-08", "2017-08-01", year_height=52)
 
     home("Bainbridge Island, WA", "1988-04-08", "2004-08-15", href="")
     home("Kathmandu, Nepal", "2004-08-18", "2005-06-15", href="")
@@ -14,9 +16,11 @@ def katie():
     home("Portland, ME", "2010-08-15", "2016-03-18", href="")
     home("Brooklyn, NY", "2016-04-01", "2017-08-01", href="")
 
-    roomate(1, "Chip", "2004-08-18", "2005-06-15")
-    roomate(2, "Mara and Darcy", "2007-09-20", "2008-05-20")
-    roomate(1, "Alison (and Sonny and Will)", "2010-09-01", "2011-06-01")
-    roomate(1, "Chris and Megan", "2011-06-01", "2012-09-01")
-    roomate(3, "Sarah", "2012-09-01", "2013-09-01")
-    roomate(2, "Johnicholas", "2013-09-01", "2017-08-01")
+    roommate(1, "Chip", "2004-08-18", "2005-06-15")
+    roommate(2, "Mara and Darcy", "2007-09-20", "2008-05-20")
+    roommate(1, "Alison (and Sonny and Will)", "2010-09-01", "2011-06-01")
+    roommate(1, "Chris and Megan", "2011-06-01", "2012-09-01")
+    roommate(3, "Sarah", "2012-09-01", "2013-09-01")
+    roommate(2, "Johnicholas", "2013-09-01", "2017-08-01")
+
+make_pico(katie, sys.argv[1:])
